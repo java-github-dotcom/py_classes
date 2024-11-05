41b8682743be64cdca5bfc38570f2f0eb48964a0
Javohir <javohir_rajabboyev@outlook.com>
2024-11-05 16:47:03 +0500

    Add files via upload

---
+++ class_5/notebook/note_operator.py
@@ -0,0 +1,15 @@
+import pandas
+from tabulate import tabulate
+
+class Base_methods():
+    def __init__():
+        pass
+class Base_operations(Base_methods):
+    pass
+
+
+
+
+
+
+
---
+++ class_5/notebook/schema.md
@@ -0,0 +1,8 @@
+note_operator.py
+    data types
+    base operations
+    Base methods
+note_ui.py
+    basic_ui
+note_archive.csv
+    this is an archive for storing the data 
